d80e1a159a4a047743f8cd6c384db02c29e91546
ishaansarna <ishaansarna23@gmail.com>
2021-11-24 16:42:03 +0530

    Removed rogue whitespace

--- src/com/ishaansarna/Admin.java
+++ src/com/ishaansarna/Admin.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class  Admin implements Runnable {
+public class Admin implements Runnable {
 
     private Question currentQuestion;
     private final List<Student> students;
