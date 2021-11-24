d470ade26f0eb986b21a46ef5bfcd879ad6176a5
ishaansarna <ishaansarna23@gmail.com>
2021-11-24 20:24:02 +0530

    Added @NotNull in CompareByRollNo

--- src/com/ishaansarna/CompareByRollNo.java
+++ src/com/ishaansarna/CompareByRollNo.java
@@ -1,10 +1,12 @@
 package com.ishaansarna;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Comparator;
 
 public class CompareByRollNo implements Comparator<Student> {
     @Override
-    public int compare(Student o1, Student o2) {
+    public int compare(@NotNull Student o1, @NotNull Student o2) {
         return Integer.compare(o1.getRollNo(), o2.getRollNo());
     }
 }
